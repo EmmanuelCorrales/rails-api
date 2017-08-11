14637fa63e360bf60094c277b9df61a735046539
Emmanuel Corrales <emmanuelcorrales22@gmail.com>
2017-08-11 10:14:17 +0800

    Check if the model quiz item has valid attributes.

--- spec/models/quiz_item_spec.rb
+++ spec/models/quiz_item_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe QuizItem, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { described_class.new }
+
+  it "is valid with valid attributes" do
+    subject.question = "What is your name?"
+    subject.answer  = "Emmanuel Corrales"
+    expect(subject).to be_valid
+  end
+
+  it "is not valid without question" do
+    subject.question= "What is your name?"
+    expect(subject).to_not be_valid
+	end
+
+  it "is not valid without an answer" do
+    subject.answer = "Emmanuel Corrales"
+    expect(subject).to_not be_valid
+  end
+
 end
